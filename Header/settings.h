746e3b21486fc5e8f7e6839e19b78658ccfe0347
t o <finalfantasytimothy@gmail.com>
2022-03-14 15:20:32 -0700

    Lite work on the settings class. Adding implementation of Save and Load

--- Header/settings.h
+++ Header/settings.h
@@ -6,15 +6,6 @@
 #include <vector>
 #include <map>
 
-#ifdef __linux__ 
-#include "Linux/EvDev.h"
-#include "Linux/connect.h"
-#elif _WIN32
-#include "Windows/winConnect.h"
-#include "Windows/Xinput.h"
-#endif
-
-#include "twitch.h"
 #include "json.hpp"
 
 using json = nlohmann::json;
@@ -26,6 +17,7 @@ class Settings
     private:
     // ToDo: Fix path handling to generate settings folder in the executeable's directory. Missing a gui libary makes finding that a challenge.
     fs::path filePath = "settings/settings.json";
+    std::fstream file;
     json j;
 
     public:
--- src/settings.cpp
+++ src/settings.cpp
@@ -1,5 +1,45 @@
 #include "settings.h"
 
+// Load Json
+bool Settings::Load()
+{
+    file.open(filePath);
+    file.exceptions(std::ifstream::eofbit | std::ifstream::failbit | std::ifstream::badbit);
+    if (file.is_open())
+    {
+        try
+        {
+            j = json::parse(file);
+        }
+        catch (std::exception const& e)
+        {
+            std::cerr << "Error: " << e.what() << std::endl;
+            return false;
+        }
+        file.close();
+    }
+    return true;
+}
+
+// Save Json
+bool Settings::Save()
+{
+    std::string settingsParsed;
+    file.open(filePath);
+    file.exceptions(std::ifstream::eofbit | std::ifstream::failbit | std::ifstream::badbit);
+    try
+    {
+        file << j.dump(4);
+        file.close();
+    }
+    catch (std::exception const& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return false;
+    }
+     return true;
+}
+
 // Could be printing settings. Or, writing the file
 std::ostream& operator<<(std::ostream &out, const Settings &c)
 {
